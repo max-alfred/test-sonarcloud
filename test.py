baba48789369ac92e8bd32ba31e0f87c60bffba1
Max.Tsai <max.tsai@alfred.camera>
2023-03-02 14:29:25 +0800

    add

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 def divide_numbers(num1, num2):
-    if num2 != 0:
+    if num2 == 0:
         return num1 / num2
     else:
         return "Division by zero error!"
         
 
-divide_numbers(2, 0)
+print(divide_numbers(2, 0))
